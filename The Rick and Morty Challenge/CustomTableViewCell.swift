aa918e48b3c869c360a406a4a5e56571a7a74912
Filipe iCarros <filipe.martins@icarros.com.br>
2024-08-17 16:54:05 -0300

    feat: iniciando criando uma tableView, criando uma custom view para cada item da lista, e criando uma lista inicial fixa para teste

---
+++ The Rick and Morty Challenge/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  The Rick and Morty Challenge
+//
+//  Created by Filipe Icarros on 17/08/24.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
