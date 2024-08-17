c7fd963e5de945e5b8c569e9f90fb35a1864e47b
Filipe iCarros <filipe.martins@icarros.com.br>
2024-08-17 16:56:26 -0300

    feat: commit incial

--- The Rick and Morty Challenge/ViewController.swift
+++ The Rick and Morty Challenge/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource {
+    
+    @IBOutlet weak var table: UITableView!
+    
+    let data: [Test] = [
+        Test(title: "test 1"),
+        Test(title: "test 2"),
+        Test(title: "test 3"),
+        Test(title: "test 4"),
+        Test(title: "test 5"),
+        Test(title: "test 6")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = data[indexPath.row]
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+        cell.label.text = item.title
+        return cell
     }
 
+}
 
+struct Test {
+    let title: String
 }
 
